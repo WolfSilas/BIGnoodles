3847e26b8127e1372e4d5812d7b4576576841d6c
Malte Stoepke <160076495+MalteStoepke@users.noreply.github.com>
2024-04-05 18:17:56 +0200

    GUI workInProgress

--- Assets/GameBehavior.cs
+++ Assets/GameBehavior.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class GameBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
+// 2
+public int MaxItems = 4;
+    // 3
+  
+    public Text HealthText;
+    public Text ItemText;
+    public Text ProgressText;
+    // 4
     void Start()
     {
-        
+        ItemText.text += _itemsCollected;
+        HealthText.text += _playerHP;
     }
-
-    // Update is called once per frame
-    void Update()
+    private int _itemsCollected = 0;
+    public int Items
     {
-        
+        get { return _itemsCollected; }
+        set
+        {
+            _itemsCollected = value;
+            // 5
+            ItemText.text = "Items Collected: " + Items;
+            // 6
+            if (_itemsCollected >= MaxItems)
+            {
+                ProgressText.text = "You've found all the items!";
+            }
+            else
+            {
+                ProgressText.text = "Item found, only " +
+                (MaxItems - _itemsCollected) + " more to go!";
+            }
+        }
+    }
+    private int _playerHP = 10;
+    public int HP
+    {
+        get { return _playerHP; }
+        set {
+           _playerHP = value;
+            // 7
+            HealthText.text = "Player Health: " + HP;
+            Debug.LogFormat("Lives: {0}", _playerHP);
+        }
     }
 }
