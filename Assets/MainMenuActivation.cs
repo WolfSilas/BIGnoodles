9cd9d48996346305096a5c3b08b909690a89f5be
Merilea <Bragheeth@gmail.com>
2024-04-05 22:00:38 +0200

    I am here to save u

--- Assets/MainMenuActivation.cs
+++ Assets/MainMenuActivation.cs
@@ -1,17 +1,63 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class MainMenuNavigation : MonoBehaviour //I attached this script to the camera bc it has to be in an object that's present throughout the scene(s)
+public class PauseMenu : MonoBehaviour
 {
+    public GameObject pauseMenuCanvas;
+    public GameObject helpPanel;
+    public Text helpText;
+
+    void Start()
+    {
+        // Ensure that the help panel is initially hidden
+        if (helpPanel != null)
+        {
+            helpPanel.SetActive(false);
+        }
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            LoadMainMenu();
+            TogglePauseMenu();
         }
     }
 
-    void LoadMainMenu()
+    public void TogglePauseMenu()
+    {
+        if (pauseMenuCanvas != null)
+        {
+            bool isPaused = !pauseMenuCanvas.activeSelf;
+            pauseMenuCanvas.SetActive(isPaused);
+            Time.timeScale = isPaused ? 0f : 1f; // Pause or resume the game accordingly
+        }
+    }
+
+    public void ShowHelp()
+    {
+        // Toggle the visibility of the help panel
+        if (helpPanel != null)
+        {
+            helpPanel.SetActive(!helpPanel.activeSelf);
+
+            // Set sample instructions in the help text
+            if (helpText != null)
+            {
+                if (helpPanel.activeSelf)
+                {
+                    helpText.text = "Sample Instructions:\n\n- Use WASD or arrow keys to move.\n- Press Spacebar to jump.\n- Press Escape to open this menu.";
+                }
+                else
+                {
+                    helpText.text = ""; // Clear the help text when hiding the panel
+                }
+            }
+        }
+    }
+
+    public void QuitToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
     }
