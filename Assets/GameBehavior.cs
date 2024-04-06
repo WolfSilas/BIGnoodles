5f68596e9213bf705281525eaaa14a035227bad8
VikiST68 <vikist.kn@gmail.com>
2024-04-06 16:00:27 +0200

    TextMesh

--- Assets/GameBehavior.cs
+++ Assets/GameBehavior.cs
@@ -1,57 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameBehavior : MonoBehaviour
 {
-// 2
-public int MaxItems = 4;
+    // Maximum number of items in the level
+    public int MaxItems = 4;
 
-    // 3
+    // Text objects to display player health, collected items, and progress
+    
+    public TextMesh HealthText;
+    public TextMesh ItemText;
+    public TextMesh ProgressText;
 
+    // Private variables to track collected items and player health
+    private int _itemsCollected = 0;
+    private int _playerHP = 10;
 
-    public Text HealthText;
-    public Text ItemText;
-    public Text ProgressText;
-  
-   
-    // 4
     void Start()
     {
-        ItemText.text += _itemsCollected;
-        HealthText.text += _playerHP;
+        // Initialize UI text with starting values
+        ItemText.text = "Items Collected: " + _itemsCollected;
+        HealthText.text = "Player Health: " + _playerHP;
     }
-    private int _itemsCollected = 0;
+
+    // Property to get and set the number of collected items
     public int Items
     {
         get { return _itemsCollected; }
         set
         {
+            // Update collected items count
             _itemsCollected = value;
-            // 5
             ItemText.text = "Items Collected: " + Items;
-            // 6
+
+            // Check if all items have been collected
             if (_itemsCollected >= MaxItems)
             {
+                // Update progress text when all items are collected
                 ProgressText.text = "You've found all the items!";
             }
             else
             {
-                ProgressText.text = "Item found, only " +
-                (MaxItems - _itemsCollected) + " more to go!";
+                // Update progress text with remaining items to collect
+                ProgressText.text = "Item found, only " + (MaxItems - _itemsCollected) + " more to go!";
             }
         }
     }
-    private int _playerHP = 10;
+// Property to get and set player health
     public int HP
     {
         get { return _playerHP; }
-        set {
-           _playerHP = value;
-            // 7
+        set
+        {
+            // Update player health
+            _playerHP = value;
             HealthText.text = "Player Health: " + HP;
             Debug.LogFormat("Lives: {0}", _playerHP);
+
+            // Check for losing condition (if health reaches zero or below)
+            if (_playerHP <= 0)
+            {
+                // Implement losing condition here (in this case, just log a message)
+                Debug.Log("Game Over - Player has lost!");
+            }
         }
     }
 }
