aa049dcf97db1ae649fd1f77028b041ae8a03804
VikiST68 <152392508+VikiST68@users.noreply.github.com>
2024-06-02 17:27:47 +0300

    implementent the day system in the new scene

--- Assets/Scripts/Random Scripts/SustainabilityBar.cs
+++ Assets/Scripts/Random Scripts/SustainabilityBar.cs
@@ -3,25 +3,4 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SustainabilityBar : MonoBehaviour
-{
-    [SerializeField] GameBehavior gb;
-    [SerializeField] Slider slider;
 
-    // Method to handle the button click event
-    public void GenerateSustainabilityAndMoveSlider()
-    {
-        // Generate sustainability (increase player HP)
-        gb.HP += 5;
-
-        // Move the slider based on the updated player HP value
-        slider.value = gb.HP;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // Set the slider value to the current player HP
-        slider.value = gb.HP;
-    }
-}
