2ebaf2a6f6733dc045d4e839c238c4151c22dbca
VikiST68 <152392508+VikiST68@users.noreply.github.com>
2024-06-05 18:34:07 +0300

    shop

--- Assets/Scripts/Shop/ShopManager.cs
+++ Assets/Scripts/Shop/ShopManager.cs
@@ -6,48 +6,39 @@ using UnityEngine.UI;
 
 public class ShopManager : MonoBehaviour
 {
-    public int tokens;   
-    public TMP_Text tokenText;
-    public ShopItemSo[] shopItemso;
-    public GameObject[] shopPanelsGo;
-    public ShopTemplate[] shopPanels;
+    public int tokens;                  //
+    public TMP_Text tokenText;          //
+    public ShopItemSo[] shopItemso;     //
+    public GameObject[] shopPanelsGo;   //
+    public ShopTemplate[] shopPanels;   //
    
-   // public GameObject shopGenerate;
-   /// private LaptopInteraction laptopInteraction; // Reference to the LaptopInteraction script
-    ///private GameObject shopCanvas; // Reference to the shop canvas
-    /// <summary>
-    /// /private CharacterController characterController;
-    /// </summary>
 
     void Start()
     {
-        //    characterController = GetComponent<CharacterController>();
-        for (int i = 0; i < shopPanelsGo.Length; i++) 
-        shopPanelsGo[i].SetActive(true);
-        tokenText.text = "Tokens" + tokens.ToString();
-        LoadPannels();
-        // UpdateEEPText();
-     //   laptopInteraction = FindObjectOfType<LaptopInteraction>(); // Find the LaptopInteraction script in the scene
-      //  shopCanvas = laptopInteraction.shopCanvas; // Get the shop canvas reference
+        for (int i = 0; i < shopPanelsGo.Length; i++)           //
+        shopPanelsGo[i].SetActive(true);                        //
+                
+        tokenText.text = "Tokens" + tokens.ToString();          //
+
+        LoadPannels();                                          //
+
     }
 
-    public void AddTokens()
+    public void AddTokens()                                     //
     {
-      //  characterController = GetComponent<CharacterController>();
 
         
-        tokens++;
-        tokenText.text = "Tokens" + tokens.ToString();
-        // CheckPurchaseable();
+        tokens++;                                               //
+        tokenText.text = "Tokens" + tokens.ToString();          //
         
     }
-    public void LoadPannels()
+    public void LoadPannels()                                                               //
     {
-        for(int i = 0; i < shopItemso.Length; i ++) 
+        for(int i = 0; i < shopItemso.Length; i ++)                                         //
         {
-            shopPanels[1].titleTxt.text = shopItemso[i].title;
-            shopPanels[1].descriptionTxt.text = shopItemso[i].description;
-            shopPanels[1].costTxt.text = "Tokens" + shopItemso[i].baseCost.ToString();
+            shopPanels[i].titleTxt.text = shopItemso[i].title;                              //
+            shopPanels[i].descriptionTxt.text = shopItemso[i].description;                  //
+            shopPanels[i].costTxt.text = "Tokens" + shopItemso[i].baseCost.ToString();      //
 
         }
     }
@@ -55,9 +46,3 @@ public class ShopManager : MonoBehaviour
 
   
 
-  //  void UpdateEEPText()
-  //  {
-   //     tokenText.text = "Tokens: " + tokens.ToString();
-   // }
-
- 
